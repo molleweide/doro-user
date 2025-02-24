77ecc9b8f3a81a7e0579019496b7e9afff538653
hjalmar jakobsson <hjalmar.jakobsson@telia.com>
2025-02-24 04:58:36 -0500

    save

--- sources/aliases2_.sh
+++ sources/aliases2_.sh
@@ -12,10 +12,17 @@ function xxee() {
   echo "???? XXX"
 }
 
+alias f="echo"
+
 alias GG="echo"
 
 alias a="echo"
+alias z="echo"
 alias b="echo"
 alias c="echo"
+alias e="echo"
+alias n="echo"
+alias i="echo"
+alias o="echo"
 alias d="echo"
 alias eeeee="echo"
