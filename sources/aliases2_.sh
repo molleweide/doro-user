85eec971a505c8f9609d681faa6e2bb7de74d5ac
hjalmar jakobsson <hjalmar.jakobsson@telia.com>
2025-02-26 16:43:49 -0500

    wip aliases collision handler

--- sources/aliases2_.sh
+++ sources/aliases2_.sh
@@ -1,33 +1,38 @@
-
-
 alias x="echo"
 alias xy="echo"
 
 alias nvim="echo"
 alias x="echo"
 
+alias a="echo"
 function ars() {
-  echo "$@"
+	echo "$@"
 }
 
-
+alias x="echo"
 
 function xxee() {
-  echo "$@"
+	echo "$@"
 }
 
 alias f="echo"
 alias GG="echo"
 alias a="echo"
+alias x="echo"
 alias un_der="echo"
 alias z="echo"
 alias x="echo"
 alias b="echo"
 alias c="echo"
+alias a="echo"
 alias e="echo"
 alias n="echo"
 alias x="echo"
+alias a="echo"
 alias i="echo"
+alias b="echo"
 alias o="echo"
 alias d="echo"
 alias eeeee="echo"
+alias e="echo"
+alias e="echo"
